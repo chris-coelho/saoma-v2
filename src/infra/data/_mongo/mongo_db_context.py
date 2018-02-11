703c3a7558b3a05127b487c7d316c15a643a9a05
Cristovao Coelho <cristovao3g@gmail.com>
2018-02-11 12:56:26 -0200

    Updated some files to solver a supported python version on Heroku

--- src/infra/data/_mongo/mongo_db_context.py
+++ src/infra/data/_mongo/mongo_db_context.py
@@ -2,7 +2,11 @@ import pymongo
 
 
 class MongoDbContext:
+
     def get_context(self, host, database_name, username, password):
-        uri = host.format(username, password)
+        if not username:
+            uri = host
+        else:
+            uri = host.format(username, password)
         client = pymongo.MongoClient(uri)
         return client[database_name]
